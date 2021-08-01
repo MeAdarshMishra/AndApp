e84c9d407bc63be77963c9c1d877edfa64eaf78d
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-02 00:08:24 +0530

    Android Application

--- app/src/main/java/com/example/andapp/TtSpeech.java
+++ app/src/main/java/com/example/andapp/TtSpeech.java
@@ -1,14 +1,51 @@
 package com.example.andapp;
 
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Locale;
 
 public class TtSpeech extends AppCompatActivity {
+    Button b1,b2;
+    EditText e1;
+    TextToSpeech t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tt_speech);
+        b1=findViewById(R.id.speech);
+        e1=findViewById(R.id.editToSpeech);
+        b2=findViewById(R.id.backspeech);
+        t=new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                t.setLanguage(Locale.ENGLISH);
+                t.setSpeechRate(0.07f);
+            }
+        });
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s1=e1.getText().toString();
+                t.speak(s1,TextToSpeech.QUEUE_FLUSH,null);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(TtSpeech.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
     }
 }
