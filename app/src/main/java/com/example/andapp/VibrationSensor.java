73caabdceeacbe6a2cfa165ab24b4e2c7c5d9d3b
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-01 23:55:12 +0530

    Android Application

---
+++ app/src/main/java/com/example/andapp/VibrationSensor.java
@@ -0,0 +1,45 @@
+package com.example.andapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Button;
+
+public class VibrationSensor extends AppCompatActivity {
+    Button b1,b2;
+    Vibrator v1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vibration_sensor);
+        b1=findViewById(R.id.clicktovibrate);
+        b2=findViewById(R.id.backvibration);
+        v1= (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Build.VERSION.SDK_INT>26)
+                {
+                    v1.vibrate(1000);
+                }
+                else
+                {
+                    v1.vibrate(1000);
+                }
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(VibrationSensor.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+    }
+}
