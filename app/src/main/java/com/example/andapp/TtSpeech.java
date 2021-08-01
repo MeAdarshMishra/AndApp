c6ac743f0846e174e8302971b22ac7b23ae0e3b5
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-02 00:05:35 +0530

    Android Application

---
+++ app/src/main/java/com/example/andapp/TtSpeech.java
@@ -0,0 +1,14 @@
+package com.example.andapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TtSpeech extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tt_speech);
+    }
+}
