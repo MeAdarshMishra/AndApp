2edba1fcdd9529ce49af3dac4f08fcfd3baad90c
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-01 23:45:39 +0530

    Android Application

--- app/src/main/java/com/example/andapp/MainActivity.java
+++ app/src/main/java/com/example/andapp/MainActivity.java
@@ -2,13 +2,66 @@ package com.example.andapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView t1;
+    Button b1,b2,b3,b4,b5,b6,b7,b8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        t1=findViewById(R.id.textView3);
+        b1=findViewById(R.id.texttospeeech);
+        b2=findViewById(R.id.vibrationbutton);
+        b3=findViewById(R.id.wifibutton);
+        b4=findViewById(R.id.accelerometerbutton);
+        b5=findViewById(R.id.MP4);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ttt=new Intent(AuthPage.this, TtSpeech.class);
+                startActivity(ttt);
+                finish();
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent vibration=new Intent(AuthPage.this,VibrationSensor.class);
+                startActivity(vibration);
+                finish();
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent wifi=new Intent(AuthPage.this,WifiPage.class);
+                startActivity(wifi);
+                finish();
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent accelerometer=new Intent(AuthPage.this,AccelerometerSensor.class);
+                startActivity(accelerometer);
+                finish();
+                //INcomplete work
+            }
+        });
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mp4=new Intent(AuthPage.this,MP4_Player.class);
+                startActivity(mp4);
+                finish();
+            }
+        });
     }
 }
