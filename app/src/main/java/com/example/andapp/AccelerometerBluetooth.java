23c19d209bcea75720ae941a10796b0af02b78e0
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-03 00:08:27 +0530

    Android Application

---
+++ app/src/main/java/com/example/andapp/AccelerometerBluetooth.java
@@ -0,0 +1,60 @@
+package com.example.andapp;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AccelerometerBluetooth extends AppCompatActivity implements SensorEventListener {
+    BluetoothAdapter bluetoothAdapter;
+    Button back;
+    SensorManager sensorManager;
+    Sensor sensor;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accelerometer_bluetooth);
+        back = findViewById(R.id.abluetoothback);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back = new Intent(AccelerometerBluetooth.this, AccelerometerSensor.class);
+                startActivity(back);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+        int x1 = (int) x;
+        int y1 = (int) y;
+        int z1 = (int) z;
+
+        if (x1 != 0 || y1 != 0 || z1 != 0) {
+            bluetoothAdapter.enable();
+        } else {
+            bluetoothAdapter.disable();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
