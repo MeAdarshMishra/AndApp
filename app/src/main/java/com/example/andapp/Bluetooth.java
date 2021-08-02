c779a722f2a4a3edcb81b62db9c312cfe0070c6d
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-03 00:47:43 +0530

    Android Application

--- app/src/main/java/com/example/andapp/Bluetooth.java
+++ app/src/main/java/com/example/andapp/Bluetooth.java
@@ -1,14 +1,53 @@
 package com.example.andapp;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Bluetooth extends AppCompatActivity {
+public class Bluetooth extends AppCompatActivity implements SensorEventListener {
+    SensorManager sensorManager;
+    Sensor sensor;
+    BluetoothAdapter bluetoothAdapter;
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth);
+        backButton = findViewById(R.id.button78);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Bluetooth.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.values[0] > 10) {
+            bluetoothAdapter.enable();
+        } else {
+            bluetoothAdapter.disable();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
