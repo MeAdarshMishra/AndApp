e1e1cffef6d0c520d3b746c8182bb80013180aec
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-02 22:57:13 +0530

    Android Application

--- app/src/main/java/com/example/andapp/AccelerometerSensor.java
+++ app/src/main/java/com/example/andapp/AccelerometerSensor.java
@@ -1,14 +1,17 @@
 package com.example.andapp;
 
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class AccelerometerSensor extends AppCompatActivity {
+    Button bluetooth, flash, wifi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accelerometer_sensor);
+
     }
 }
