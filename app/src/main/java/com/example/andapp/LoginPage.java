380775f14b03f23814e1442a54d3ba96e51261fc
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-03 20:47:29 +0530

    Android Application

--- app/src/main/java/com/example/andapp/LoginPage.java
+++ app/src/main/java/com/example/andapp/LoginPage.java
@@ -1,14 +1,24 @@
 package com.example.andapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginPage extends AppCompatActivity {
+    Button b1, b2;
+    TextView t1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        b1 = findViewById(R.id.online);
+        b2 = findViewById(R.id.offline);
+        t1 = findViewById(R.id.logintext);
+
+
     }
 }
