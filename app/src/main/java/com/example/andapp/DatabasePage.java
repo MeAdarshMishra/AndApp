ca9d4f900ebdf5362efa1cc7bd6337a432f1b55f
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-04 00:01:14 +0530

    Android Application

--- app/src/main/java/com/example/andapp/DatabasePage.java
+++ app/src/main/java/com/example/andapp/DatabasePage.java
@@ -1,14 +1,55 @@
 package com.example.andapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class DatabasePage extends AppCompatActivity {
+    EditText e1, e2, e3, e4;
+    Button b1;
+    TextView t1;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_database_page);
+        setContentView(R.layout.activity_main);
+        t1 = findViewById(R.id.RealtimedatabaseTextview);
+        e1 = findViewById(R.id.nametextdatabase);
+        e2 = findViewById(R.id.passwordtextdatabase);
+        e3 = findViewById(R.id.emaildatabase);
+        e4 = findViewById(R.id.numberdatabase);
+        b1 = findViewById(R.id.savedatabase);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference = firebaseDatabase.getReference("users");
+                String s1 = e1.getText().toString();
+                String s2 = e2.getText().toString();
+                String s3 = e3.getText().toString();
+                String s4 = e4.getText().toString();
+                if (s4.length() != 10) {
+                    Toast.makeText(DatabasePage.this, "Enter a valid Phone number", Toast.LENGTH_SHORT).show();
+                } else {
+                    Users users = new Users(s1, s2, s3, s4);
+                    databaseReference.child(s4).setValue(users);
+                    Toast.makeText(DatabasePage.this, "Database Saved", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(DatabasePage.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        });
     }
 }
