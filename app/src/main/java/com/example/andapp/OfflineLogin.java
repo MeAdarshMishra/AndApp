6eb9259a9d34d26ed38099c53044abdb5fa04a6f
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-03 21:18:35 +0530

    Android Application

--- app/src/main/java/com/example/andapp/OfflineLogin.java
+++ app/src/main/java/com/example/andapp/OfflineLogin.java
@@ -1,10 +1,13 @@
 package com.example.andapp;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class OfflineLogin extends AppCompatActivity {
+    Button b1, b2, b3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
