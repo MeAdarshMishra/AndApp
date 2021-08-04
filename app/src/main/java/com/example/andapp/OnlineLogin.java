b4dac58182f829d8ff10313b5a59c03358f463da
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-04 22:31:35 +0530

    Android Application

--- app/src/main/java/com/example/andapp/OnlineLogin.java
+++ app/src/main/java/com/example/andapp/OnlineLogin.java
@@ -10,12 +10,20 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.annotations.NotNull;
 
 public class OnlineLogin extends AppCompatActivity {
     TextView t1;
