7f50ab17bf2189f149cbffe1e1d3fcf5b89593d6
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-04 21:46:11 +0530

    Android Application

--- app/src/main/java/com/example/andapp/OnlineLogin.java
+++ app/src/main/java/com/example/andapp/OnlineLogin.java
@@ -1,14 +1,80 @@
 package com.example.andapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 public class OnlineLogin extends AppCompatActivity {
+    TextView t1;
+    SignInButton signInButton;
+    FirebaseAuth firebaseAuth;
+    GoogleSignInClient googleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_online_login);
+        t1 = findViewById(R.id.textView2);
+        signInButton = findViewById(R.id.OnlineSignin);
+        firebaseAuth = FirebaseAuth.getInstance();
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken("152607254109-hl7sn5e4r1df5b6fjq0chi89kv4fq2gv.apps.googleusercontent.com").requestEmail().build();
+        googleSignInClient = GoogleSignIn.getClient(OnlineLogin.this, googleSignInOptions);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = googleSignInClient.getSignInIntent();
+                startActivityForResult(i, 33);
+            }
+        });
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser != null) {
+            Intent j = new Intent(OnlineLogin.this, AllApp.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(j);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 33) {
+            Task<GoogleSignInAccount> signInAccountTask = GoogleSignIn.getSignedInAccountFromIntent(data);
+            if (signInAccountTask.isSuccessful()) {
+                Toast.makeText(this, "Google SignIn Done", Toast.LENGTH_SHORT).show();
+                try {
+                    GoogleSignInAccount googleSignInAccount = signInAccountTask.getResult(ApiException.class);
+                    if (googleSignInAccount != null) {
+                        AuthCredential authCredential = GoogleAuthProvider.getCredential(googleSignInAccount.getIdToken(), null);
+                        firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this, "Firebase Updated", Toast.LENGTH_SHORT).show();
+                                    Intent k = new Intent(MainActivity.this, Second.class).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    startActivity(k);
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Firebase not Updated", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                } catch (ApiException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
