814eef3b543dc895ed38b897a7d40cb2adfd4ade
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-04 21:38:05 +0530

    Android Application

---
+++ app/src/main/java/com/example/andapp/OnlineLogin.java
@@ -0,0 +1,14 @@
+package com.example.andapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OnlineLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_online_login);
+    }
+}
