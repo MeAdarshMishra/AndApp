55a1fde24a9319640884a2e422585028fb61f975
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-06 00:05:22 +0530

    Android Application

---
+++ app/src/main/java/com/example/andapp/Internet.java
@@ -0,0 +1,41 @@
+package com.example.andapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Internet extends AppCompatActivity {
+    WebView webView;
+    EditText editText;
+    Button web, back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_internet);
+        webView = findViewById(R.id.Viewweb);
+        editText = findViewById(R.id.textweb);
+        web = findViewById(R.id.searchweb);
+        back = findViewById(R.id.webback);
+        web.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = editText.getText().toString();
+                webView.loadUrl(s);
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back = new Intent(Internet.this, AllApp.class);
+                startActivity(back);
+                finish();
+            }
+        });
+    }
+}
