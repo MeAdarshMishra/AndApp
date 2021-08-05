fab2cb940e391321a2486fde4f73189c57a7066e
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-05 18:16:09 +0530

    Android Application

--- app/src/main/java/com/example/andapp/Bluetooth.java
+++ app/src/main/java/com/example/andapp/Bluetooth.java
@@ -1,20 +1,14 @@
 package com.example.andapp;
 
 import android.bluetooth.BluetoothAdapter;
-import android.content.Intent;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Bluetooth extends AppCompatActivity implements SensorEventListener {
-    SensorManager sensorManager;
-    Sensor sensor;
+public class Bluetooth extends AppCompatActivity {
+    ToggleButton toggleButton;
     BluetoothAdapter bluetoothAdapter;
     Button back;
 
@@ -23,31 +17,14 @@ public class Bluetooth extends AppCompatActivity implements SensorEventListener 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth);
         back = findViewById(R.id.blueback);
+        toggleButton = findViewById(R.id.toggleButton);
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Bluetooth.this, MainActivity.class);
-                startActivity(i);
-                finish();
+        toggleButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                bluetoothAdapter.enable();
+            } else {
+                bluetoothAdapter.disable();
             }
         });
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (event.values[0] > 10) {
-            bluetoothAdapter.enable();
-        } else {
-            bluetoothAdapter.disable();
-        }
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
     }
 }
