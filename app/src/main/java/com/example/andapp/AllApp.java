ba839b4125d7ffe0b0d45b3f714cd20b2a27d523
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-05 15:27:11 +0530

    Android Application

--- app/src/main/java/com/example/andapp/AllApp.java
+++ app/src/main/java/com/example/andapp/AllApp.java
@@ -1,14 +1,28 @@
 package com.example.andapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AllApp extends AppCompatActivity {
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_app);
+        b1 = findViewById(R.id.allappbackbutton);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AllApp.this, OnlineLogin.class);
+                startActivity(intent)
+                ;
+                finish();
+            }
+        });
     }
 }
