8809fb77c246002da0bd1dbdf81873d060fcbc94
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-05 19:04:26 +0530

    Android Application

--- app/src/main/java/com/example/andapp/FlashLight.java
+++ app/src/main/java/com/example/andapp/FlashLight.java
@@ -1,60 +1,61 @@
 package com.example.andapp;
 
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+import android.content.Intent;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class FlashLight extends AppCompatActivity implements
-        SensorEventListener {
-    SensorManager sensorManager;
-    Sensor sensor;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class FlashLight extends AppCompatActivity {
     CameraManager cameraManager;
-    Button backButton;
+    Button backButton, on, off;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash_light);
-        backButton=findViewById(R.id.button67);
-        cameraManager=(CameraManager)getSystemService(CAMERA_SERVICE);
-        cameraManager = (CameraManager)getSystemService(CAMERA_SERVICE);
-        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-    }
+        on = findViewById(R.id.button);
+        off = findViewById(R.id.button2);
+        backButton = findViewById(R.id.backflash);
+        cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (event.values[0] > 10) {
-            try {
-                String cameraIdForFlashlight = cameraManager.getCameraIdList()[0];
-                cameraManager.setTorchMode(cameraIdForFlashlight, true);
-            } catch (CameraAccessException exception) {
-                System.out.println(exception);
+        on.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                try {
+                    String cameraIdForFlashlight = cameraManager.getCameraIdList()[0];
+                    cameraManager.setTorchMode(cameraIdForFlashlight, true);
+                } catch (CameraAccessException exception) {
+                    System.out.println(exception);
+                }
             }
-        } else {
-            try {
-                String cameraIdForFlashlight = cameraManager.getCameraIdList()[0];
-                cameraManager.setTorchMode(cameraIdForFlashlight, false);
-            } catch (CameraAccessException exception) {
-                System.out.println(exception);
+        });
+
+        off.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                try {
+                    String cameraIdForFlashlight = cameraManager.getCameraIdList()[0];
+                    cameraManager.setTorchMode(cameraIdForFlashlight, false);
+                } catch (CameraAccessException exception) {
+                    System.out.println(exception);
+                }
             }
-        }
-
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back = new Intent(FlashLight.this, AllApp.class);
+            }
+        });
     }
 }
+
