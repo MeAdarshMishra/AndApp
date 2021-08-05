4bc09017c539defe750431a573d20d5ae338c02f
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-06 00:35:45 +0530

    Android Application

---
+++ app/src/main/java/com/example/andapp/MediaPlayermp3.java
@@ -0,0 +1,14 @@
+package com.example.andapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MediaPlayermp3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_media_playermp3);
+    }
+}
