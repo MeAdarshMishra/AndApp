98311e94ce3831e174919334f8e10a0f80cefa80
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-06 00:49:56 +0530

    Android Application

--- app/src/main/java/com/example/andapp/MediaPlayermp3.java
+++ app/src/main/java/com/example/andapp/MediaPlayermp3.java
@@ -1,14 +1,36 @@
 package com.example.andapp;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MediaPlayermp3 extends AppCompatActivity {
+    Button play, pause, stop, back;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media_playermp3);
+        play = findViewById(R.id.button140);
+        pause = findViewById(R.id.button141);
+        stop = findViewById(R.id.button142);
+        back = findViewById(R.id.button143);
+        mediaPlayer = MediaPlayer.create(this, R.raw.believer);
+        play.setOnClickListener(v -> mediaPlayer.start());
+        pause.setOnClickListener(v -> mediaPlayer.pause());
+        stop.setOnClickListener(v -> mediaPlayer.stop());
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back = new Intent(MediaPlayermp3.this, AllApp.class);
+                startActivity(back);
+                finish();
+            }
+        });
     }
 }
