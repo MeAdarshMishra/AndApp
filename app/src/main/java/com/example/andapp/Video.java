a1fb4cbd60d6bd3ff1ec5575dbd7d1700dc7e7f8
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-08 02:10:00 +0530

    Android Application

---
+++ app/src/main/java/com/example/andapp/Video.java
@@ -0,0 +1,14 @@
+package com.example.andapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Video extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+    }
+}
