a4a475be443f7c9b07ba7ca33be97db96f20d026
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-08 03:05:30 +0530

    Android Application

--- app/src/main/java/com/example/andapp/Other.java
+++ app/src/main/java/com/example/andapp/Other.java
@@ -1,14 +1,55 @@
 package com.example.andapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Other extends AppCompatActivity {
+    Button flash, blue, tt, back, vibration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+        flash = findViewById(R.id.flashother);
+        blue = findViewById(R.id.bluetoothother);
+        vibration = findViewById(R.id.vibrationother);
+        tt = findViewById(R.id.ttother);
+        back = findViewById(R.id.backother);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back = new Intent(Other.this, AllApp.class);
+                startActivity(back);
+                finish();
+            }
+        });
+        flash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent flashp = new Intent(Other.this, FlashLight.class);
+                startActivity(flashp);
+                finish();
+            }
+        });
+        blue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent bluep = new Intent(Other.this, Bluetooth.class);
+                startActivity(bluep);
+                finish();
+            }
+        });
+        tt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ttp = new Intent(Other.this, TtSpeech.class);
+                startActivity(ttp);
+                finish();
+            }
+        });
     }
 }
