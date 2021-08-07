436ff371b8da6e47ccfb1588e3ea4aacce976a80
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-08 02:15:29 +0530

    Android Application

--- app/src/main/java/com/example/andapp/Video.java
+++ app/src/main/java/com/example/andapp/Video.java
@@ -1,14 +1,50 @@
 package com.example.andapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Video extends AppCompatActivity {
+    VideoView videoView;
+    Button video, back;
+    MediaController mediaController;
+    Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+        videoView = findViewById(R.id.videoviewcamera);
+        video = findViewById(R.id.videorecorderopen);
+        back = findViewById(R.id.videoback);
+        mediaController = new MediaController(this);
+        video.setOnClickListener(v -> {
+            Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+            startActivityForResult(intent, 100);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uri = data.getData();
+
+        videoView.setVideoURI(uri);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+        videoView.start();
+        back.setOnClickListener(v -> {
+            Intent back = new Intent(Video.this, Camerapage.class);
+            startActivity(back);
+            finish();
+        });
+
     }
 }
