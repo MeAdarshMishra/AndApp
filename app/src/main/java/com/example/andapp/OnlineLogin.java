e5c34d4ff9dbfb3a54e8b7d3806f2adf127d4957
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2021-08-08 19:42:10 +0530

    Update

--- app/src/main/java/com/example/andapp/OnlineLogin.java
+++ app/src/main/java/com/example/andapp/OnlineLogin.java
@@ -3,6 +3,7 @@ package com.example.andapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,6 +31,7 @@ public class OnlineLogin extends AppCompatActivity {
     SignInButton signInButton;
     FirebaseAuth firebaseAuth;
     GoogleSignInClient googleSignInClient;
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +40,15 @@ public class OnlineLogin extends AppCompatActivity {
         t1 = findViewById(R.id.WelcomehomePagebutton);
         signInButton = findViewById(R.id.OnlineSignin);
         firebaseAuth = FirebaseAuth.getInstance();
+        back = findViewById(R.id.googleloginback);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent back = new Intent(OnlineLogin.this, MainActivity.class);
+                startActivity(back);
+                finish();
+            }
+        });
         GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken("326693301584-1gs9i7bffsqbfems9udqa5dpnu2dlars.apps.googleusercontent.com").requestEmail().build();
         googleSignInClient = GoogleSignIn.getClient(OnlineLogin.this, googleSignInOptions);
         signInButton.setOnClickListener(new View.OnClickListener() {
